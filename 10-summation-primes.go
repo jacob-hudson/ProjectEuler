c64830382dcc9d1deec58d7dd2cbc86f1edbe121
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-08-22 19:56:53 -0500

    Problem 10

---
+++ 10-summation-primes.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+    arr := make([]bool, 2000000)
+    arr[0], arr[1] = true, true
+    sum, prime := int64(5), 3
+    var k int
+    for {
+        for k = 2 * prime; k < len(arr); k += prime {
+            arr[k] = true
+        }
+        for k = prime + 2; k < len(arr) && arr[k]; k += 2 {
+        }
+        if k < len(arr) {
+            prime = k
+            sum += int64(k)
+        } else {
+            break
+        }
+    }
+    println(sum)
+}
