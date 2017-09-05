df5f09357e9c1e0c190411bdd3b5a7a86a859771
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-09-04 21:11:34 -0500

    problem 19

---
+++ python/19-counting-sundays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from datetime import date
+
+sundays=0
+for year in range(1901,2001):
+	for month in range(1,13):
+		if date(year,month,1).weekday()==6:
+			sundays+=1
+print sundays
