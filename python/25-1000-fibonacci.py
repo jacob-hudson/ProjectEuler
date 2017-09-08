2840db955105748e4f4e6c33376dfe8541646f77
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-09-07 19:23:52 -0500

    problem 25

---
+++ python/25-1000-fibonacci.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+fib_list = [0,1]
+
+i = 2
+
+while 1:
+
+	fib_new = fib_list[i-1]+fib_list[i-2]
+	fib_list.append(fib_new)
+	if fib_new>10**999:
+		print i
+		break
+	i += 1
